b9d33d48e7d3c23eea652d3dcebe0154e259baa5
Ghulam Murtaza <ghulammurtazagopang3@gmail.com>
2023-03-01 14:46:42 +0500

    4th screen added

---
+++ lib/screens/productDetailScreen/product_detail_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  const ProductDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
